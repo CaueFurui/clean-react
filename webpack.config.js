b22168299cbd824bedde7ddc7ad65e2e93093501
cauefurui <cauefurui@gmail.com>
2022-05-04 18:34:16 -0300

    chore: config sass

--- webpack.config.js
+++ webpack.config.js
@@ -10,15 +10,40 @@ module.exports = {
     filename: 'bundle.js'
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js'],
+    extensions: ['.ts', '.tsx', '.js', '.scss'],
     alias: {
       '@': path.join(__dirname, 'src')
     }
   },
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/
+      }, {
+        test: /\.scss$/,
+        use: [{
+          loader: 'style-loader'
+        },
+        {
+          loader: 'css-loader',
+          options: {
+            modules: true
+          }
+        },
+        {
+          loader: 'scss-loader'
+        }]
+      }
+    ]
+  },
   devServer: {
-    contentBase: './public',
-    writeToDisk: true,
-    historyApiFallback: true
+    static: './public',
+    historyApiFallback: true,
+    devMiddleware: {
+      writeToDisk: true
+    }
   },
   externals: {
     react: 'React',
